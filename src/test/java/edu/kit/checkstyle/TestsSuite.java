0ab9bbcc10ca27f5a91ed06b181f577cda2a7839
Simon Schaefer <mail@antoras.de>
2013-07-18 21:44:24 +0200

    Add TestsSuite

---
+++ src/test/java/edu/kit/checkstyle/TestsSuite.java
@@ -0,0 +1,29 @@
+package edu.kit.checkstyle;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import edu.kit.checkstyle.checks.CallHierarchyComplexityCheckTest;
+import edu.kit.checkstyle.checks.DiscouragedMethodCallCheckTest;
+import edu.kit.checkstyle.checks.InstanceofUsageCheckTest;
+import edu.kit.checkstyle.checks.StaticUsageCheckTest;
+import edu.kit.checkstyle.checks.metrics.AttributesPerClassCheckTest;
+import edu.kit.checkstyle.checks.metrics.MethodsPerClassCheckTest;
+import edu.kit.checkstyle.checks.metrics.NumberOfClassesCheckTest;
+import edu.kit.checkstyle.checks.metrics.NumberOfStatementsPerMethodCheckTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+  CallHierarchyComplexityCheckTest.class,
+  DiscouragedMethodCallCheckTest.class,
+  InstanceofUsageCheckTest.class,
+  StaticUsageCheckTest.class,
+  AttributesPerClassCheckTest.class,
+  MethodsPerClassCheckTest.class,
+  NumberOfClassesCheckTest.class,
+  NumberOfStatementsPerMethodCheckTest.class
+})
+public class TestsSuite {
+
+}
